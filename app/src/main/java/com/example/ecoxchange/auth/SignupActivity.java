12fd8ed43db1f192d136b2b0aab7182b25e768ab
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-01 15:15:06 +0200

    first commit

---
+++ app/src/main/java/com/example/ecoxchange/auth/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecoxchange.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecoxchange.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
