7fde61ddfa69723e095ba7b388f17ff6bf0884e7
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-09 13:40:31 +0200

    finish design screens

---
+++ app/src/main/java/com/example/ecoxchange/home/MyPostsActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecoxchange.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecoxchange.R;
+
+public class MyPostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_posts);
+    }
+}
